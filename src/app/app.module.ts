1358647c82d0cc603addbbe8590d20af1a817b20
casi-le <43793275+casi-le@users.noreply.github.com>
2023-12-26 11:58:36 -0700

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EventsComponent } from './events/events.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    EventsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
