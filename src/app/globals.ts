f51a391e2a14528c532179136b4aea297bc9ea43
casi-le <43793275+casi-le@users.noreply.github.com>
2025-04-04 18:29:24 -0600

    several homepage updates

---
+++ src/app/globals.ts
@@ -0,0 +1,2 @@
+export const ftdLink ="https://go.rallyup.com/topdog2025";
+export const ftdDate="September 2025"
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
 
+import { faThumbsUp as faThumbsUp} from '@fortawesome/free-solid-svg-icons';
+import { faPaw as faPaw} from '@fortawesome/free-solid-svg-icons';
+import { faFaceSmileWink as faFaceSmileWink} from '@fortawesome/free-solid-svg-icons';
+
+
+import * as myGlobals from '../globals'; //<==== this one (**Updated**)
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(library: FaIconLibrary) {
+    // Add multiple icons to the library
+    library.addIcons(faThumbsUp, faPaw, faFaceSmileWink);
+  }
+  public ftdDate=myGlobals.ftdDate;
+  public ftdLink = myGlobals.ftdLink;
 }
