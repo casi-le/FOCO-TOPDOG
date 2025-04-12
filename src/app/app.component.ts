360767c1bccd3e1fb2ca207f5e5e3054ecc1b42c
casi-le <43793275+casi-le@users.noreply.github.com>
2025-04-12 17:37:52 -0600

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ContactUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
