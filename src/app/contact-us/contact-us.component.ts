c38eb3973b53ac9244541db97830ef047a2a1f4b
casi-le <43793275+casi-le@users.noreply.github.com>
2025-04-16 13:43:56 -0600

    css fixes

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -19,15 +19,15 @@ export class ContactUsComponent {
     var email:string = this.profileForm.get('email')?.value!;
     var name:string = this.profileForm.get('name')?.value!;
 
-
-    this.emailService.sendEmail(name, email, '').subscribe(
-      response => {
-        console.log('Email sent successfully!');
-      },
-      error => {
-        console.log('Error sending email:', error);
-      }
-    );
+    window.open('mailto:focotopdog@gmail.com?cc='+email+'?subject=FOCO Top Dog Contact by '+name);
+    // this.emailService.sendEmail(name, email, '').subscribe(
+    //   response => {
+    //     console.log('Email sent successfully!');
+    //   },
+    //   error => {
+    //     console.log('Error sending email:', error);
+    //   }
+    // );
 
   }
 }
