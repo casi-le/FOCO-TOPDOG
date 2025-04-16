b3f60fb0db399d6527ca741555ed9370e35536d5
casi-le <43793275+casi-le@users.noreply.github.com>
2025-04-16 10:09:19 -0600

    added blank form contact us

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,11 +1,22 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('',  Validators.email)
+  })
+  onSubmit(){
+    var email = this.profileForm.get('email')?.value;
+    var name = this.profileForm.get('name')?.value;
+    window.open('mailto:'+email);
 
+  }
 }
