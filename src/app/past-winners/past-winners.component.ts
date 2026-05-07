136ac5e39fcafe6ab7b1f2cbd4dc9c960212c4fe
casi-le <43793275+casi-le@users.noreply.github.com>
2026-05-07 14:29:54 -0600

    added photos

--- src/app/past-winners/past-winners.component.ts
+++ src/app/past-winners/past-winners.component.ts
@@ -11,8 +11,8 @@ export class PastWinnersComponent {
   pastwinnersInfo=[
     {'name':'Washburne', 'year': '2021', 'label':'Washburne Label.png', 'photo':'washburne_photo.jpg'},
     {'name': 'Knox', 'year': '2022', 'label':'Knox-Top-Dog-Label.png', 'photo':'knox_photo.jpg'},
-    {'name':'Blue', 'year':'2023', 'label':'Blue 2023 Label.png', 'photo':''},
-    {'name':'Zirkel', 'year':'2024', 'label':'2024 Zirkel.jpg', 'photo':''},
+    {'name':'Blue', 'year':'2023', 'label':'Blue 2023 Label.png', 'photo':'blue_photo.jpg'},
+    {'name':'Zirkel', 'year':'2024', 'label':'2024 Zirkel.jpg', 'photo':'zirkel_photo.jpg'},
     {'name':'Rocco', 'year':'2025', 'label':'2025 Rocco.jpg', 'photo':''}
   ];
 
